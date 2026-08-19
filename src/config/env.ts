1d8711096a65088332083853d930e7fad9247ee9
MoacirMSJ <moacirmascarenha86@gmail.com>
2026-08-19 02:33:58 -0300

    feat: add cache on get news

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export const env = {
   port: process.env.PORT || 3000,
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/api-teste-g4f',
+  redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
 };
